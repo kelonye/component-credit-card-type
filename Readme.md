536ca4b7e03331c67868b900258f374c91ceb504
Mitchel Kelonye <kelonyemitchel@gmail.com>
2014-01-03 23:59:51 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,34 @@
+Credit card types
+---
+
+- visa
+- master-card
+- american-express
+- diners-club
+- discover
+- jcb
+
+Install
+---
+
+    $ npm install -g component
+    $ component install kelonye/component-credit-card
+
+Example
+---
+
+```javascript
+
+console.log(require('component-credit-card')('4444444444444448');
+
+```
+
+Test
+---
+
+    $ make test
+
+Licence
+---
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,14 @@
+var card = require('component-credit-card');
+
+describe('cards:', function() {
+  it('should return undefined on unsupported card number', function() {
+    assert.equal(card('00'), undefined);
+  });
+  it('should return right card type on supported card number', function() {
+    for (var cardType in fixtures){
+      fixtures[cardType].forEach(function(cardNumber){
+        assert.equal(card(cardNumber), cardType);
+      });
+    }
+  });
+});
